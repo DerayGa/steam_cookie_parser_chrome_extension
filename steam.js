43b298f2c0c48a28d36879e778d68ccd35db183d
deray <deray.hsueh@SSIRs-Mac-Pro.local>
2016-03-02 14:12:18 +0800

    auto open steamcommunity webpage, when it's not exist

--- steam.js
+++ steam.js
@@ -1,37 +1,60 @@
 var id, login, cookie;
 
 function getCookies(domain, name, callback) {
-    chrome.cookies.get({"url": domain, "name": name}, function(cookie) {
-        if(callback) {
-            callback(cookie.value);
-        }
-    });
+  chrome.cookies.get({
+    "url": domain,
+    "name": name
+  }, function(cookie) {
+    if (callback) {
+      callback(cookie.value);
+    }
+  });
 }
 
-function showContent(){
-	if(!id || !login) return;
+function showContent() {
+  if (!id || !login) return;
 
-	cookie.value = 'sessionid = "' + id + '"\r\nsteamLogin = "' + login + '"\r\nsteamparental = ""\r\nsort = ""';
+  cookie.value = 'sessionid = "' + id + '"\r\nsteamLogin = "' +
+    login + '"\r\nsteamparental = ""\r\nsort = ""';
 }
+
+function showCookie() {
+
+  cookie = document.getElementById("cookie");
+  cookie.onmousedown = function() {
+    cookie.focus();
+
+    document.execCommand('SelectAll');
+
+    document.execCommand("Copy", false, null);
+  }
+
+  getCookies("http://steamcommunity.com", "sessionid", function(value) {
+    id = value;
+
+    showContent();
+  });
+
+  getCookies("http://steamcommunity.com", "steamLogin", function(value) {
+    login = value;
+
+    showContent();
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-	cookie = document.getElementById("cookie");
-	cookie.onmousedown = function(){
-		cookie.focus();
 
-	    document.execCommand('SelectAll');
+  chrome.tabs.getAllInWindow(null, function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+      if (tabs[i].url.indexOf('steamcommunity.com') > -1) {
+        showCookie();
+        return;
+      }
+    }
 
-	    document.execCommand("Copy", false, null);
-	}
-
-	getCookies("http://steamcommunity.com", "sessionid", function(value) {
-		id = value;
-		
-		showContent();
-	});
-
-	getCookies("http://steamcommunity.com", "steamLogin", function(value) {
-		login = value;
-		
-		showContent();
-	});	
+    var steamcommunityURL = "http://steamcommunity.com/";
+    chrome.tabs.create({
+      url: steamcommunityURL
+    });
+  });
 });
